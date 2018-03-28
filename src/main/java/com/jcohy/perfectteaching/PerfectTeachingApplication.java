171f662106c9f72b3c442445d5c355269a280f0c
jiachao <jia_chao23@126.com>
2018-03-29 07:38:20 +0800

    Initial commit

---
+++ src/main/java/com/jcohy/perfectteaching/PerfectTeachingApplication.java
@@ -0,0 +1,12 @@
+package com.jcohy.perfectteaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectTeachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectTeachingApplication.class, args);
+	}
+}
